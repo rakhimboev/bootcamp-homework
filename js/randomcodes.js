49d1eace1ffc3a2c8e0485e7887c9b76d21a18ae
rakhimboev <dilshodbekrakhimbaev@gmail.com>
2024-09-29 17:23:03 -0400

    Week 4 Day 2 Homework

--- js/randomcodes.js
+++ js/randomcodes.js
@@ -1,18 +1,43 @@
 function generateCode() {
-  var code = "";
-  var str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$";
+    var code = " ";
+    var getCode = " ";
+    var btnvalue;
+    var str =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$";
 
-  for (i = 1; i <= 8; i++) {
-    var char = Math.random() * str.length;
-    code += str.charAt(char);
+    for (i = 1; i <= 8; i++) {
+      var char = Math.random() * str.length;
+      code += str.charAt(char);
+    }
+    return code;
   }
-  return code;
-}
 
-document.getElementById("codes").innerHTML = generateCode();
+  document.getElementById("codes").innerHTML = generateCode();
 
-function disableButton() {
-     document.getElementById("submit").disabled = true;
-}
+  function disableButton(btnvalue) {
+    document.getElementById("submit").disabled = btnvalue;
+    if (btnvalue == true) {
+      document.getElementById("submit").style.backgroundColor =
+        "rgba(73,119,209,0.3)";
+      document.getElementById("submit").style.color =
+        "rgba(255,255,255,0.5)";
+    } else {
+      document.getElementById("submit").style.backgroundColor =
+        "rgba(73,119,209,1)";
+      document.getElementById("submit").style.color = "rgba(255,255,255,1)";
+    }
+  }
 
-disableButton()
+  disableButton();
+
+  var codebox = document.getElementById("codeentered");
+  codebox.addEventListener("input", evaluateCode);
+
+  function evaluateCode() {
+    getCode = document.getElementById("codeentered").value;
+    var charset1 = getCode.trim();
+    var charset2 = code.trim();
+    if (charset1.length == charset2.length && charset1 == charset2) {
+      disableButton(false);
+    }
+  }
